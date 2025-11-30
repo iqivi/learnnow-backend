286fcdb0a81182afcf1e5ad17710283a0a704cd4
jakubg1 <24206305+jakubg1@users.noreply.github.com>
2025-11-30 12:30:46 +0100

    Add repository classes

---
+++ src/main/java/com/learnnow/notification/repository/NotificationRepository.java
@@ -0,0 +1,9 @@
+package com.learnnow.notification.repository;
+
+import com.learnnow.notification.model.Notification;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface NotificationRepository extends JpaRepository<Notification, Long> {
+}
---
+++ src/main/java/com/learnnow/session/repository/SessionRepository.java
@@ -0,0 +1,9 @@
+package com.learnnow.session.repository;
+
+import com.learnnow.session.model.Session;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SessionRepository extends JpaRepository<Session, Long> {
+}
